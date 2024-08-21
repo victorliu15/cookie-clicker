753f9f48c7e9af77d70b0fc0f9d111bdf2c6f7a9
Victor Liu <victorliu5000@gmail.com>
2024-08-21 17:59:06 -0400

    environment set up

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        barlow: ['"barlow"', "sans-serif"],
+        roboto: ['"roboto mono"', "sans-serif"],
+      },
+    },
+  },
+};
