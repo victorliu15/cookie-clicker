44395b82cf58bae194a6a2124322e7c65d19f935
Victor Liu <victorliu5000@gmail.com>
2024-08-22 19:43:38 -0400

    normal upgrades done

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ document
     let upgradeCount = document.getElementById("normalClickUpgradeCount");
     upgradeCount.innerHTML = normalUpgradeCount;
   });
+
+let superClickCount = 0;
+document
+  .getElementById("superClickUpgrade")
+  .addEventListener("click", function () {
+    superClickCount += 1;
+    let upgradeCount = document.getElementById("superClickUpgradeCount");
+    upgradeCount.innerHTML = superClickCount;
+  });
+
+let criticalClickCount = 0;
+document
+  .getElementById("criticalClickUpgrade")
+  .addEventListener("click", function () {
+    criticalClickCount += 1;
+    let upgradeCount = document.getElementById("criticalClickUpgradeCount");
+    upgradeCount.innerHTML = criticalClickCount;
+  });
+
+let epicClickCount = 0;
+document
+  .getElementById("epicClickUpgrade")
+  .addEventListener("click", function () {
+    epicClickCount += 1;
+    let upgradeCount = document.getElementById("epicClickUpgradeCount");
+    upgradeCount.innerHTML = epicClickCount;
+  });
+
+let legendaryClickCount = 0;
+document
+  .getElementById("legendaryClickUpgrade")
+  .addEventListener("click", function () {
+    legendaryClickCount += 1;
+    let upgradeCount = document.getElementById("legendaryClickUpgradeCount");
+    upgradeCount.innerHTML = legendaryClickCount;
+  });
