aa49401e453c7859450acfb5a1daa58b62915c01
Victor Liu <victorliu5000@gmail.com>
2024-08-24 13:39:15 -0400

    tools added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let cookieMultiplier = 1;
+localStorage.setItem("cookieClicks", 0);
+
 document.getElementById("cookie").addEventListener("click", function () {
   let counter = 0;
   if (parseInt(localStorage.getItem("cookieClicks")) > 0) {
@@ -17,9 +20,9 @@ document.getElementById("cookie").addEventListener("click", function () {
     temp += clickUpgrades[i - 1] * (i + 1) ** 2;
   }
   if (temp > 0) {
-    counter += temp;
+    counter += temp * cookieMultiplier;
   } else {
-    counter += 1;
+    counter += cookieMultiplier;
   }
   localStorage.setItem("cookieClicks", counter);
   let cookieCounts = document.getElementById("counter");
@@ -58,7 +61,8 @@ updateUpgrade("premiumAutoclickUpgradeCount", "premiumAutoclickerCount");
 updateUpgrade("vipAutoclickUpgradeCount", "vipAutoclickerCount");
 
 setInterval(() => {
-  if (parseInt(localStorage.getItem("cookieClicks")) > 0) {
+  console.log(cookieMultiplier);
+  if (parseInt(localStorage.getItem("cookieClicks")) >= 0) {
     let autoclickerUpgrades = [
       parseInt(localStorage.getItem("basicAutoclickerCount") || 0),
       parseInt(localStorage.getItem("normalAutoclickerCount") || 0),
@@ -68,9 +72,9 @@ setInterval(() => {
     ];
     let autoclickerCount = 0;
     for (let i = 0; i < autoclickerUpgrades.length; i++) {
-      autoclickerCount += autoclickerUpgrades[i] * (i + 2) ** 2;
+      autoclickerCount +=
+        autoclickerUpgrades[i] * (i + 2) ** 2 * cookieMultiplier;
     }
-    console.log(autoclickerCount);
     let temp = parseInt(localStorage.getItem("cookieClicks"));
     temp += autoclickerCount;
     localStorage.setItem("cookieClicks", temp);
@@ -78,3 +82,253 @@ setInterval(() => {
     cookieCounts.innerHTML = "Clicks: " + localStorage.getItem("cookieClicks");
   }
 }, 1000);
+
+document.getElementById("fistTool").addEventListener("click", function () {
+  let fist = document.getElementById("fistTool");
+  let fistStatus = document.getElementById("fistStatus");
+  if (fistStatus.innerHTML.includes("clicks")) {
+    fist.style.backgroundColor = "green";
+    fistStatus.innerHTML = "equipped";
+    cookieMultiplier = 2;
+    if (document.getElementById("stickStatus").innerHTML === "equipped") {
+      document.getElementById("stickStatus").innerHTML = "equip";
+      document.getElementById("stickTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("rockStatus").innerHTML === "equipped") {
+      document.getElementById("rockStatus").innerHTML = "equip";
+      document.getElementById("rockTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("gunStatus").innerHTML === "equipped") {
+      document.getElementById("gunStatus").innerHTML = "equip";
+      document.getElementById("gunTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("nukeStatus").innerHTML === "equipped") {
+      document.getElementById("nukeStatus").innerHTML = "equip";
+      document.getElementById("nukeTool").style.backgroundColor = "white";
+    }
+  } else if (fistStatus.innerHTML.includes("equipped")) {
+    fist.style.backgroundColor = "white";
+    fistStatus.innerHTML = "equip";
+    cookieMultiplier = 1;
+  } else {
+    fist.style.backgroundColor = "green";
+    fistStatus.innerHTML = "equipped";
+    cookieMultiplier = 2;
+    if (document.getElementById("stickStatus").innerHTML === "equipped") {
+      document.getElementById("stickStatus").innerHTML = "equip";
+      document.getElementById("stickTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("rockStatus").innerHTML === "equipped") {
+      document.getElementById("rockStatus").innerHTML = "equip";
+      document.getElementById("rockTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("gunStatus").innerHTML === "equipped") {
+      document.getElementById("gunStatus").innerHTML = "equip";
+      document.getElementById("gunTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("nukeStatus").innerHTML === "equipped") {
+      document.getElementById("nukeStatus").innerHTML = "equip";
+      document.getElementById("nukeTool").style.backgroundColor = "white";
+    }
+  }
+});
+
+document.getElementById("stickTool").addEventListener("click", function () {
+  let stick = document.getElementById("stickTool");
+  let stickStatus = document.getElementById("stickStatus");
+  if (stickStatus.innerHTML.includes("clicks")) {
+    stick.style.backgroundColor = "green";
+    stickStatus.innerHTML = "equipped";
+    cookieMultiplier = 5;
+    if (document.getElementById("fistStatus").innerHTML === "equipped") {
+      document.getElementById("fistStatus").innerHTML = "equip";
+      document.getElementById("fistTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("rockStatus").innerHTML === "equipped") {
+      document.getElementById("rockStatus").innerHTML = "equip";
+      document.getElementById("rockTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("gunStatus").innerHTML === "equipped") {
+      document.getElementById("gunStatus").innerHTML = "equip";
+      document.getElementById("gunTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("nukeStatus").innerHTML === "equipped") {
+      document.getElementById("nukeStatus").innerHTML = "equip";
+      document.getElementById("nukeTool").style.backgroundColor = "white";
+    }
+  } else if (stickStatus.innerHTML.includes("equipped")) {
+    stick.style.backgroundColor = "white";
+    stickStatus.innerHTML = "equip";
+    cookieMultiplier = 1;
+  } else {
+    stick.style.backgroundColor = "green";
+    stickStatus.innerHTML = "equipped";
+    cookieMultiplier = 5;
+    if (document.getElementById("fistStatus").innerHTML === "equipped") {
+      document.getElementById("fistStatus").innerHTML = "equip";
+      document.getElementById("fistTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("rockStatus").innerHTML === "equipped") {
+      document.getElementById("rockStatus").innerHTML = "equip";
+      document.getElementById("rockTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("gunStatus").innerHTML === "equipped") {
+      document.getElementById("gunStatus").innerHTML = "equip";
+      document.getElementById("gunTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("nukeStatus").innerHTML === "equipped") {
+      document.getElementById("nukeStatus").innerHTML = "equip";
+      document.getElementById("nukeTool").style.backgroundColor = "white";
+    }
+  }
+});
+
+document.getElementById("rockTool").addEventListener("click", function () {
+  let rock = document.getElementById("rockTool");
+  let rockStatus = document.getElementById("rockStatus");
+  if (rockStatus.innerHTML.includes("clicks")) {
+    rock.style.backgroundColor = "green";
+    rockStatus.innerHTML = "equipped";
+    cookieMultiplier = 10;
+    if (document.getElementById("fistStatus").innerHTML === "equipped") {
+      document.getElementById("fistStatus").innerHTML = "equip";
+      document.getElementById("fistTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("stickStatus").innerHTML === "equipped") {
+      document.getElementById("stickStatus").innerHTML = "equip";
+      document.getElementById("stickTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("gunStatus").innerHTML === "equipped") {
+      document.getElementById("gunStatus").innerHTML = "equip";
+      document.getElementById("gunTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("nukeStatus").innerHTML === "equipped") {
+      document.getElementById("nukeStatus").innerHTML = "equip";
+      document.getElementById("nukeTool").style.backgroundColor = "white";
+    }
+  } else if (rockStatus.innerHTML.includes("equipped")) {
+    rock.style.backgroundColor = "white";
+    rockStatus.innerHTML = "equip";
+    cookieMultiplier = 1;
+  } else {
+    rock.style.backgroundColor = "green";
+    rockStatus.innerHTML = "equipped";
+    cookieMultiplier = 10;
+    if (document.getElementById("fistStatus").innerHTML === "equipped") {
+      document.getElementById("fistStatus").innerHTML = "equip";
+      document.getElementById("fistTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("stickStatus").innerHTML === "equipped") {
+      document.getElementById("stickStatus").innerHTML = "equip";
+      document.getElementById("stickTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("gunStatus").innerHTML === "equipped") {
+      document.getElementById("gunStatus").innerHTML = "equip";
+      document.getElementById("gunTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("nukeStatus").innerHTML === "equipped") {
+      document.getElementById("nukeStatus").innerHTML = "equip";
+      document.getElementById("nukeTool").style.backgroundColor = "white";
+    }
+  }
+});
+
+document.getElementById("gunTool").addEventListener("click", function () {
+  let gun = document.getElementById("gunTool");
+  let gunStatus = document.getElementById("gunStatus");
+  if (gunStatus.innerHTML.includes("clicks")) {
+    gun.style.backgroundColor = "green";
+    gunStatus.innerHTML = "equipped";
+    cookieMultiplier = 100;
+    if (document.getElementById("fistStatus").innerHTML === "equipped") {
+      document.getElementById("fistStatus").innerHTML = "equip";
+      document.getElementById("fistTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("stickStatus").innerHTML === "equipped") {
+      document.getElementById("stickStatus").innerHTML = "equip";
+      document.getElementById("stickTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("rockStatus").innerHTML === "equipped") {
+      document.getElementById("rockStatus").innerHTML = "equip";
+      document.getElementById("rockTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("nukeStatus").innerHTML === "equipped") {
+      document.getElementById("nukeStatus").innerHTML = "equip";
+      document.getElementById("nukeTool").style.backgroundColor = "white";
+    }
+  } else if (gunStatus.innerHTML.includes("equipped")) {
+    gun.style.backgroundColor = "white";
+    gunStatus.innerHTML = "equip";
+    cookieMultiplier = 1;
+  } else {
+    gun.style.backgroundColor = "green";
+    gunStatus.innerHTML = "equipped";
+    cookieMultiplier = 100;
+    if (document.getElementById("fistStatus").innerHTML === "equipped") {
+      document.getElementById("fistStatus").innerHTML = "equip";
+      document.getElementById("fistTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("stickStatus").innerHTML === "equipped") {
+      document.getElementById("stickStatus").innerHTML = "equip";
+      document.getElementById("stickTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("rockStatus").innerHTML === "equipped") {
+      document.getElementById("rockStatus").innerHTML = "equip";
+      document.getElementById("rockTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("nukeStatus").innerHTML === "equipped") {
+      document.getElementById("nukeStatus").innerHTML = "equip";
+      document.getElementById("nukeTool").style.backgroundColor = "white";
+    }
+  }
+});
+
+document.getElementById("nukeTool").addEventListener("click", function () {
+  let nuke = document.getElementById("nukeTool");
+  let nukeStatus = document.getElementById("nukeStatus");
+  if (nukeStatus.innerHTML.includes("clicks")) {
+    nuke.style.backgroundColor = "green";
+    nukeStatus.innerHTML = "equipped";
+    cookieMultiplier = 1000;
+    if (document.getElementById("fistStatus").innerHTML === "equipped") {
+      document.getElementById("fistStatus").innerHTML = "equip";
+      document.getElementById("fistTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("stickStatus").innerHTML === "equipped") {
+      document.getElementById("stickStatus").innerHTML = "equip";
+      document.getElementById("stickTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("rockStatus").innerHTML === "equipped") {
+      document.getElementById("rockStatus").innerHTML = "equip";
+      document.getElementById("rockTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("gunStatus").innerHTML === "equipped") {
+      document.getElementById("gunStatus").innerHTML = "equip";
+      document.getElementById("gunTool").style.backgroundColor = "white";
+    }
+  } else if (nukeStatus.innerHTML.includes("equipped")) {
+    nuke.style.backgroundColor = "white";
+    nukeStatus.innerHTML = "equip";
+    cookieMultiplier = 1;
+  } else {
+    nuke.style.backgroundColor = "green";
+    nukeStatus.innerHTML = "equipped";
+    cookieMultiplier = 1000;
+    if (document.getElementById("fistStatus").innerHTML === "equipped") {
+      document.getElementById("fistStatus").innerHTML = "equip";
+      document.getElementById("fistTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("stickStatus").innerHTML === "equipped") {
+      document.getElementById("stickStatus").innerHTML = "equip";
+      document.getElementById("stickTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("rockStatus").innerHTML === "equipped") {
+      document.getElementById("rockStatus").innerHTML = "equip";
+      document.getElementById("rockTool").style.backgroundColor = "white";
+    }
+    if (document.getElementById("gunStatus").innerHTML === "equipped") {
+      document.getElementById("gunStatus").innerHTML = "equip";
+      document.getElementById("gunTool").style.backgroundColor = "white";
+    }
+  }
+});
